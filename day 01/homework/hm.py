9b0ddf315d476d810018adb12a86dbd35ae0d4be
aleksandre1908 <azadvorni@gmail.com>
2025-07-13 16:05:55 +0400

    “განახლება”

---
+++ day 01/homework/hm.py
@@ -0,0 +1,23 @@
+# შექმენით 4 ცვლადი და მასში შეინახეთ თქვენი ოჯახის წევრების სახელები, შემდეგ დაპრინტეთ 
+print("aleksandre")
+print("maria")
+print("inga")
+print("nata")
+print("irina")
+print("nikoloz")
+print("bebo larisa")
+
+
+#ეს არის 4 სხვადასხვა ცვლადის მნიშვნელობა
+
+name = "aleksandre"
+age = 12
+heiting = 1.52
+surname = "zadvorni"
+
+# დაბეჭვდა
+
+print("aleksandre")
+print(12)
+print(1.52)
+print("zadvorni")
